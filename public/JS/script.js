2b8b924b0bb6953bb1f75b114ff3e28d9ffc9d08
Katt <katt@pruvot.eu>
2024-10-21 19:54:59 +0200

    Peaufinage de la page FAQ

--- public/JS/script.js
+++ public/JS/script.js
@@ -2,11 +2,15 @@ var scrollTexts = document.querySelectorAll('.faq_text');
 
 scrollTexts.forEach((scrollText) => {
     scrollText.addEventListener('click', () => {
-        const hiddenText = scrollText.nextElementSibling.nextElementSibling; // Récupérer le texte caché
+        var hiddenText = scrollText.nextElementSibling.nextElementSibling; // Récupérer le texte caché
+        var arrow_icon = scrollText.nextElementSibling;
+
         if (hiddenText.style.display == 'none' || hiddenText.style.display == '') {
             hiddenText.style.display = 'block'; // Afficher le texte déroulant
+            arrow_icon.textContent = "↑";
         } else {
             hiddenText.style.display = 'none'; // Masquer le texte déroulant
+            arrow_icon.textContent = "↓";
         }
     });
 });
